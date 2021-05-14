22e64dcdb3894fb36826cdb415ff44b5979a4121
Yunhye-Choi <anffl98@gmail.com>
2021-05-14 20:42:13 +0900

    유네파트는 진짜 끝인듯!

--- app/src/main/java/com/kw/yuseyun_2020/Guidance.java
+++ app/src/main/java/com/kw/yuseyun_2020/Guidance.java
@@ -6,6 +6,21 @@ public class Guidance {
     private int nodeID;
     private int direction;
     private String index; // 출발, 도착, 1, 2, 3, 4 .. 저장
+    private boolean passed;
+
+    public String getSpeechSentence() {
+        return speechSentence;
+    }
+
+    private String speechSentence;
+
+    public void setPassed(boolean passed) {
+        this.passed = passed;
+    }
+
+    public boolean isPassed() {
+        return passed;
+    }
 
     public String getIndex() {
         return index;
@@ -19,6 +34,13 @@ public class Guidance {
         this.sentence = sentence;
         this.nodeID = nodeID;
         this.direction = direction;
+        if (direction == -2) {
+            this.speechSentence = "경로 안내를 시작합니다.";
+        } else if (direction == -3) {
+            this.speechSentence = "목적지 도착입니다. 경로 안내를 종료합니다.";
+        } else {
+            this.speechSentence = "잠시 후 " + sentence + "입니다.";
+        }
     }
 
     public int getDirection() {
@@ -42,9 +64,9 @@ public class Guidance {
         return nodeID;
     }
 
-    public void setNodeID(int nodeID) {
+    /*public void setNodeID(int nodeID) {
         this.nodeID = nodeID;
-    }
+    }*/
 
     @Override
     public String toString () {
